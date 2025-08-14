dc489021123d976faa2b74da043cc16f48fa8b6f
VieraJulian <julianviera2019@hotmail.com>
2025-08-14 10:09:28 -0300

    feat(structure): initialize project layout with base dependencies

---
+++ users/src/test/java/com/mentorlink/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorlink.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
