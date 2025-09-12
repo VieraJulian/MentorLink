03a8e8696c3f14acf1f953a12138157824cbb94f
VieraJulian <julianviera2019@hotmail.com>
2025-09-12 17:03:59 -0300

    feat(gateway): configure routing and migrate to reactive stack with WebFlux

--- api-gateway/src/main/java/com/mentorlink/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/mentorlink/api_gateway/config/SecurityConfig.java
@@ -2,14 +2,14 @@ package com.mentorlink.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationConverter jwtAuthenticationConverter;
@@ -19,14 +19,21 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(http -> http.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth ->
-                        oauth.jwt(jwt ->
-                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/users/**").authenticated()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtSpec -> jwtSpec
+                                .jwtAuthenticationConverter(
+                                        new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter))
+                        )
+                )
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
     }
 }
